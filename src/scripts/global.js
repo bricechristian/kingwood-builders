fde2212099e0812719ea6b3f14dc977754916b2f
Brice Christian <bricechristian93@gmail.com>
2023-04-13 23:50:40 -0400

    Setup basic form

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -36,3 +36,35 @@ if(document.querySelector(".shift-up") !== null){
         });        
     })
 }
+//SCALE IN
+if(document.querySelector(".scale-in") !== null){
+    document.querySelectorAll(".scale-in").forEach(el => {
+        let delay = 0;
+        if(el.getAttribute("data-delay")){
+            delay = Number(el.getAttribute("data-delay"));
+        }
+        inView(el, ({ target }) => {
+            animate(
+                target,
+                { scale: ["1.05", "1"], opacity: ["0", "1"], filter: ["blur(10px)", "blur(0px)"]},
+                { duration: 0.75, delay, easing: [0.25, 1, 0.5, 1] }
+            );
+        });        
+    })
+}
+//FADE IN
+if(document.querySelector(".fade-in") !== null){
+    document.querySelectorAll(".fade-in").forEach(el => {
+        let delay = 0;
+        if(el.getAttribute("data-delay")){
+            delay = Number(el.getAttribute("data-delay"));
+        }
+        inView(el, ({ target }) => {
+            animate(
+                target,
+                { opacity: ["0", "1"]},
+                { duration: 0.75, delay, easing: [0.25, 1, 0.5, 1] }
+            );
+        });        
+    })
+}
